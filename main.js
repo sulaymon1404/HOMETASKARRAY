2afd5d80733410d0f9a53e8afe01123af6adeac4
Sulaymon <sulaymonmakhsumow@gmail.com>
2023-01-21 14:24:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,169 @@
+// task1
+
+// let arr=[100,17,-2,13,54]
+// let arr2=arr.find(function(a){
+//     return a
+// })
+// console.log(arr2)
+
+// task2 
+
+// let arr=[-500,0,50]
+// let arr2=arr.reverse()
+// [0]
+// console.log(arr2)
+
+
+// task3
+
+// let a=(1,2)
+// let arr=a.split(" ")
+// console.log(arr) fewadsadawd
+
+
+
+// task4
+
+// let arr=[10,20,15]
+
+// let arr2=arr.slice(5,)
+// console.log(arr2)
+
+
+
+// task5
+
+// let arr=[22,8,14,99,103]
+// let arr2=arr.reverse()
+// console.log(arr2)
+
+
+// task6
+
+// let arr=["dffie","jnf","ifen","car"]
+// let arr2=arr.indexOf("car")
+// console.log(arr2)
+
+
+
+// task7
+
+// let arr=["Fsd","fds","DSf","fsd","Dsf"]
+// let arr2=arr.toString()
+// console.log(arr2)frefwedada
+
+
+// task8
+
+
+// let arr=[1,"ads",5,3,"ad",9]
+// let arr2=arr.map(function(element,index,array){
+//     if(typeof element != "string"){
+//         return element
+//     }
+// })
+// console.log(arr2) fsme
+
+
+
+// task9
+
+// let arr1=[1,2,3,4,5]
+// let arr2=[5,6,7,8,9]
+// let arr3=arr1.concat(arr2)
+// console.log(arr3)
+
+
+// task10
+
+// const arr=[2,5,8,3,4]
+// let arr2=arr.sort(function(a,b){
+//     return a-b
+// })
+// let min=arr2[0]
+// let arr3=arr.sort(function(a,b){
+//     return b-a
+// })
+// let max=arr2[0]
+
+
+// console.log([min,max])
+
+
+// task11
+
+// let arr=[true,5,10,false,"wad"]
+// let arr2=arr.map(function(element,index,array){
+//     return typeof element
+// })
+// console.log(arr2)
+
+
+// task12
+
+// let arr=[2,4,"EFs",123,5,"fas"]
+// let arr2=arr.map(function(element,index,array){
+//     if(typeof element != "string"){
+//        element=element.toString()
+//     }
+//     return  element
+// })
+// console.log(arr2)
+
+
+
+// task13
+
+// let arr=[1,2,3,4,5]
+// let sum=0
+// let arr2=arr.map(function(element,index,array){
+//      sum+=element
+//      let result=
+//      return result
+// })
+// console.log(arr2) 
+
+// frefwerfrw3rwre
+
+
+
+// task14
+// let arr=["google","apple","microsoft"]
+// let arr2=arr.sort(function(a,b){
+//     return a.length-b.length
+// })
+// console.log(arr2)
+
+
+// task15
+
+// let arr=[10,-2,1,-4,70,11,100]
+// let arr2=arr.sort(function(a,b){
+//     return a-b
+// })
+// console.log(arr2)
+
+
+// task16
+
+// let arr=[false,true,true,false]
+// let cnt=0
+// let arr2=arr.map(function(element,index,array){
+//     if(element==true){
+//         cnt++
+//     }
+//     return cnt
+// })
+// console.log(arr2) fajskbfkab sf
+
+
+
+
+
+
+
+
+
+
+
+
